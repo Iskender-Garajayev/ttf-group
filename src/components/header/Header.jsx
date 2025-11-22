b6b6d69ae0738667f85d65f8b4d6ce2141861d65
Iskender Garajayev <yoloten.isa@gmail.com>
2025-11-22 13:39:50 +0300

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,43 +1,3 @@
-import React from 'react';
-// import Logo from '../../imgLogo/logo.png';
-// import './header.css';
-// import { Link } from 'react-router-dom';
-
-// const Header = () => {
-//   return (
-//     <header className="header">
-//       <div className="inner-header">
-//         <div className="header-logo">
-//           <Link to={'/'}>
-//             <img src={Logo} alt="" />
-//           </Link>
-//         </div>
-//         <div className="header-navbar">
-//           <ul>
-//             <li>
-//               <Link to={'/sedan/'}>Легковые</Link>
-//             </li>
-//             <li>
-//               <Link to={'/gruzovoy/'}>Микроавтобусы</Link>
-//             </li>
-//             <li>
-//               <Link to={'/lodka/'}>Лодки</Link>
-//             </li>
-//             <li>
-//               <Link to={'/pricep/'}>Прицепы</Link>
-//             </li>
-//           </ul>
-//         </div>
-//         <div className="header-number">
-//           <h3>+375 44 554 75 75</h3>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -48,7 +8,7 @@ import Logo from '../../imgLogo/logo.png';
 function Header() {
   return (
     <>
-      <Navbar bg="light" expand="lg" data-bs-theme="light">
+      <Navbar bg="light" expand="lg" data-bs-theme="light" fixed="top">
         <Container>
           <Navbar.Brand className=" w-12 rounded-3xl" href="/">
             <img src={Logo} alt="" />
